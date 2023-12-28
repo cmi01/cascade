770a9739f51325d6f6dd6bd7a85d209c27884a95
cmi01 <40479488+cmi01@users.noreply.github.com>
2023-12-28 14:38:35 -0600

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,1 +1,47 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('ball', 'assets/ball.png');
+}
+
+var ball;
+var cursors;
+
+function create() {
+    ball = this.physics.add.sprite(400, 300, 'ball');
+    ball.setBounce(1);
+    ball.setCollideWorldBounds(true);
+    ball.setVelocity(100, 200);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        ball.setVelocityX(-160);
+    }
+    else if (cursors.right.isDown) {
+        ball.setVelocityX(160);
+    }
+    else {
+        ball.setVelocityX(0);
+    }
+}
